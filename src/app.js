f906c3afed9b72dba7b6b2224772f6b6f2dbcc9e
smmmaria39-wq <smmmaria39@gmail.com>
2026-09-12 10:03:34 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(helmet());
 // 2. CORS Configuration
 const corsOptions = {
  origin: [
-  'https://mmaccounts.store', // Your main user frontend
+  'https://smmmaria39-wq.github.io/MARIA-ADMIN', // Your main admin frontend
   'https://smmaria.site',    // Your main user frontend 
   'https://adminmaria.gt.tc'
  ],
